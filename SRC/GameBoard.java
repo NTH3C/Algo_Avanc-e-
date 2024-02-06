643f94c5351a1514cf461ea09b4c13a5e9b7c22f
NTH3C <147485340+NTH3C@users.noreply.github.com>
2024-02-06 15:22:18 +0100

    GameBoard generation started

--- SRC/GameBoard.java
+++ SRC/GameBoard.java
@@ -1,3 +1,32 @@
 public class GameBoard {
 
+    int[][] matrice;
+
+    public GameBoard(int[][] matrice) {
+        this.matrice = matrice;
+    }
+
+    public int[][] getMatrice() {
+        return matrice;
+    }
+
+    public void setMatrice(int[][] matrice) {
+        this.matrice = matrice;
+    }
+
+    public void matriceGenerator(int width, int heigth){
+        for ( int i = 0; i <= heigth; i++){
+            for ( int j = 0; j <= width; j++){
+                if ((i == 0) || (i == heigth) ){
+                    this.matrice[i][j] = 1;
+                }
+                else if ((j == 0) || (j == width)){
+                    this.matrice[i][j] = 1;
+                }
+                else {
+                    this.matrice[i][j] = 0;
+                }
+            }
+        }
+    }
 }
